8f9a67357e92523823c2efb071ea4632b4f8952c
Jeroen Smeets <jeroen@jeroensmeets.net>
2016-11-21 13:01:33 +0100

    had ux content in a js file

--- MentionsView.js
+++
@@ -1,8 +0,0 @@
-<Page ux:Class="MentionsPage">
-
-  <Router ux:Dependency="router" />
-  <JavaScript File="MentionsView.js" />
-
-  <Text Margin="0,64,0,0" Alignment="Center" Font="Italic" TextColor="#ffffff">No posts found.</Text>
-
-</Page>
